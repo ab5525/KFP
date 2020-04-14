d9d78ec9c6a4bcd23f53f4309bd851576e638d9a
Andrew <batuure15@gmail.com>
2020-04-14 13:38:42 -0400

    first commit

---
+++ src/homepage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const Homepage = () => (
+
+    <div className ='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className="title">Homepage</h1>
+                    <span className='subtitle'>Kenny - coming soon</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className="title">TITLE</h1>
+                    <span className='subtitle'>wiw</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className="title">TITLE</h1>
+                    <span className='subtitle'>wiw</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+);
+
+
+export default Homepage;
